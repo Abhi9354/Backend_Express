fd1c8f3188870d439a1f860e281f3a86a569a775
knowledge <abhi7ty4@gmail.com>
2024-04-24 23:14:47 +0530

    done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.get('/about', (req, res) => {
+    const data = {
+        name: 'John Doe',
+        age: 30
+    }
+    res.send(data)
+})
+
+app.post('/',(req,res)=>{
+    res.send('mthod post')
+})
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+})
